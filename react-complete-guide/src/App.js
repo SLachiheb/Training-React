584fd188faef1123f46fa32566f57c23d16ea763
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-01 14:57:27 +0100

    using the useState

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,40 +1,37 @@
 import "./App.css";
-import React from "react";
+import React, { useState } from "react";
 import Person from "./Person/Person";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      persons: [
-        { name: "Max", age: 28 },
-        { name: "Manu", age: 29 },
-        { name: "Stephanie", age: 26 },
-      ],
-    };
-  }
+const App = (props) => {
+  const [p, setPersons] = useState({
+    persons: [
+      { name: "Max", age: 28 },
+      { name: "Manu", age: 29 },
+      { name: "Stephanie", age: 26 },
+    ],
+  });
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi, I'm a React App</h1>
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-        >
-          My hobbie: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
-      </div>
-    );
-  }
-}
+  const switchNameHandler = () => {
+    return setPersons({
+      persons: [
+        { name: "Maximilian", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 27 },
+      ],
+    });
+  };
+
+  return (
+    <div className="App">
+      <h1>Hi, I'm a React App</h1>
+      <Person name={p.persons[0].name} age={p.persons[0].age} />
+      <Person name={p.persons[1].name} age={p.persons[1].age}>
+        My hobbie: Racing
+      </Person>
+      <Person name={p.persons[2].name} age={p.persons[2].age} />
+      <button onClick={switchNameHandler}>Switch Name</button>
+    </div>
+  );
+};
 
 export default App;
