a954fa40a032f0614623b6761ce36b9583eb45db
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-01 13:55:18 +0100

    props and state

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,17 +2,39 @@ import "./App.css";
 import React from "react";
 import Person from "./Person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hi, I'm a React App</h1>
-      <Person name="Max" age="28" />
-      <Person name="Manu" age="29">
-        My hobbie: Racing
-      </Person>
-      <Person name="Stephanie" age="26" />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [
+        { name: "Max", age: 28 },
+        { name: "Manu", age: 29 },
+        { name: "Stephanie", age: 26 },
+      ],
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a React App</h1>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        />
+        <Person
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
+        >
+          My hobbie: Racing
+        </Person>
+        <Person
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
