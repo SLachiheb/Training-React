7954badd74c256781e60be626cd037afe409769e
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-06 14:32:25 +0100

    rendering adjacent jsx elements

--- react-complete-guide/src/Components/Persons/Person/Person.js
+++ react-complete-guide/src/Components/Persons/Person/Person.js
@@ -1,19 +1,38 @@
 import React, { Component } from 'react';
 import style from './Person.module.css';
+import Aux from '../../hoc/Aux';
 
 class Person extends Component {
   render() {
     console.log('[Person.js] rendering...');
     return (
-      <div className={style.Person}>
+      <Aux className={style.Person}>
         <p onClick={this.props.click}>
           I'm a {this.props.name} and I am {this.props.age} years old!
         </p>
         <p>{this.props.children}</p>
         <input type="text" onChange={this.props.changed}></input>
-      </div>
+      </Aux>
     );
   }
 }
 
 export default Person;
+
+// return [
+//   <p onClick={this.props.click}>
+//     I'm a {this.props.name} and I am {this.props.age} years old!
+//   </p>,
+//   <p>{this.props.children}</p>,
+//   <input type="text" onChange={this.props.changed}></input>,
+// ];
+
+// return (
+//   <div className={style.Person}>
+//     <p onClick={this.props.click}>
+//       I'm a {this.props.name} and I am {this.props.age} years old!
+//     </p>
+//     <p>{this.props.children}</p>
+//     <input type="text" onChange={this.props.changed}></input>
+//   </div>
+// );
