52e19ee14dc26a24081cf32fde1f53f2eccf1cad
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-11 15:25:42 +0100

    creating a http request to get data

---
+++ react-http-web/src/containers/Blog/Blog.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import axios from "axios";
+
+import Post from "../../components/Post/Post";
+import FullPost from "../../components/FullPost/FullPost";
+import NewPost from "../../components/NewPost/NewPost";
+import "./Blog.css";
+
+class Blog extends Component {
+  state = {
+    posts: [],
+  };
+
+  async componentDidMount() {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    this.setState({ posts: response.data });
+  }
+
+  render() {
+    const posts = this.state.posts.map((post) => {
+      return <Post key={post.id} title={post.title}></Post>;
+    });
+
+    return (
+      <div>
+        <section className="Posts">{posts}</section>
+        <section>
+          <FullPost />
+        </section>
+        <section>
+          <NewPost />
+        </section>
+      </div>
+    );
+  }
+}
+
+export default Blog;
