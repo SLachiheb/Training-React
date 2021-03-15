ada49618695796b63949cdc2fe0c675b38a2e3cf
Sarah Lachiheb <sarah.lachiheb@gmail.com>
2021-03-15 16:41:19 +0100

    handling the 404 case

--- react-http-web/src/containers/Blog/Blog.js
+++ react-http-web/src/containers/Blog/Blog.js
@@ -6,6 +6,10 @@ import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
+  state = {
+    auth: false,
+  };
+
   render() {
     return (
       <div className={style.Blog}>
@@ -39,9 +43,12 @@ class Blog extends Component {
           </nav>
         </header>
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? (
+            <Route path="/new-post" component={NewPost} />
+          ) : null}
           <Route path="/posts" component={Posts} />
-          <Redirect from="/" to="/posts" />
+          <Route render={() => <h1> Not found </h1>} />
+          {/* <Redirect from="/" to="/posts" /> */}
         </Switch>
       </div>
     );
